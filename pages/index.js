5b5b1315cfac4c7ff2ec397f27d658904b0e059b
Eang Hengty <hengty174@gmail.com>
2025-04-29 01:34:31 +0700

    [add comment]

--- pages/index.js
+++ pages/index.js
@@ -1,20 +1,33 @@
 import { useState } from "react";
-import { ReceiptText,Users,DollarSign } from "lucide-react";
+import { ReceiptText,Users,DollarSign, PartyPopper } from "lucide-react";
 export default function Home(){
   const [bill,setBill]=useState('');
   const [people, setPeople]=useState('');
   const [tip,setTip]=useState('');
   const [result,setResult]=useState(null);
-
+  /**
+   * - Converts people input string into a number
+   * - Divides total bill by number of people to get how much each person must pay
+   */
   const handleSplit =()=>{
+    /**If tip input is empty → treat it like 0.
+     * So app will not crash even if user doesn't type anything for tip.
+     */
     const total=parseFloat(bill)+(parseFloat(tip)||0);
     const split=total/parseInt(people);
+    /**
+     * 	- Rounds the split result to 2 decimal places (money format like 25.00)
+     * - Saves the result into result state so that React can display it on the page
+     */
     setResult(split.toFixed(2));
   };
 
   return(
 
     <div className="min-h-screen flex flex-col items-center justify-center bg-pink-100 p-4">
+      <div className="animate-bounce mb-6">
+        <PartyPopper size={80} className="text-pink-600"></PartyPopper>
+      </div>
       <h1 className="text-4xl font-bold text-pink-800 mb-6">Easy Bill Splitter - Eang Hengty</h1>
       <div className="flex flex-col gap-4 w-full max-w-sm">
         <div className="flex items-center gap-2 border rounded p-3 bg-white">
